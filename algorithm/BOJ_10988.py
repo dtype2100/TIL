6cd31543a0f0788e7df2001c4cb61529e12036f2
datatype2100 <datatype2100.datatype@gmail.com>
2022-02-21 18:05:17 +0900

    BOJ_1000.py

---
+++ algorithm/BOJ_10988.py
@@ -0,0 +1,8 @@
+word = input()
+
+
+if word == word[::-1]:
+    print(0)
+else:
+    print(1)
+
---
+++ algorithm/BOJ_2558.py
@@ -0,0 +1,4 @@
+a = map(int, input().split())
+b = map(int, input().split())
+print(a + b)
+print(a + b)
