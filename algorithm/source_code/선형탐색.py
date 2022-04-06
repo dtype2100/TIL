36ed489ed5c1d80050bf0787d518b309df63be92
dtype2100@gmail.com <datatype2100@gmail.com>
2022-04-06 22:39:41 +0900

    Algorithm

--- algorithm/source_code/선형탐색.py
+++ algorithm/source_code/선형탐색.py
@@ -3,7 +3,7 @@ def linear_search(element, some_list):
         if some_list[i] == element:
             return i
     return None
-    # 코드를 작성하세요.
+
 
 print(linear_search(2, [2, 3, 5, 7, 11]))
 print(linear_search(0, [2, 3, 5, 7, 11]))
